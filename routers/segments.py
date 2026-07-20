4914c3a6e06fcc9ad22f60f2459bae1c20a4d990
Egor Komarov <egor.anakonda10@mail.ru>
2026-07-20 19:55:17 +0300

    исправление названия функции

--- routers/segments.py
+++ routers/segments.py
@@ -5,7 +5,7 @@ router = APIRouter(prefix="/segments", tags=["segments"])
 
 
 @router.get("/")
-def get_flights(limit: int = Query(50, ge=1, le=1000),
+def get_segments(limit: int = Query(50, ge=1, le=1000),
                 page: int = Query(1, ge=1, le=1000)):
     with get_db_context() as cursor:
         cursor.execute("""SELECT flight_id
