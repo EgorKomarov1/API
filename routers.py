20e97e668d27f8059844c33e0e7e7edeb80b06ca
Egor Komarov <egor.anakonda10@mail.ru>
2026-07-21 20:27:31 +0300

    замена group by на distinct в get_segments

--- routers.py
+++ routers.py
@@ -125,11 +125,10 @@ def get_routes(limit: int = Query(50, ge=1, le=1000),
 def get_segments(limit: int = Query(50, ge=1, le=1000),
                  page: int = Query(1, ge=1)):
     with get_db_context() as cursor:
-        cursor.execute("""SELECT flight_id
+        cursor.execute("""SELECT DISTINCT flight_id
                                 ,fare_conditions
                                 ,price
                           FROM bookings.segments
-                          GROUP BY flight_id, fare_conditions, price
                           ORDER BY price
                           LIMIT %s OFFSET %s""", (limit, (page - 1) * limit)
                        )
