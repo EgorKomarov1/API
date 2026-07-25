14fd849485a39572f07ff713435ad24adffa4a3a
Egor Komarov <egor.anakonda10@mail.ru>
2026-07-25 15:13:08 +0300

    обновление местонахождения .env

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
-    model_config = SettingsConfigDict(env_file=Path(__file__).parent / ".env")
+    model_config = SettingsConfigDict(env_file=Path(__file__).parent.parent / ".env")
 
     def database_url(self):
         return (f"postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
