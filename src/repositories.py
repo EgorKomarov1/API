cc3366ad2968736a221ef3a43c41f1ab55a02979
Egor Komarov <egor.anakonda10@mail.ru>
2026-08-06 16:05:30 +0300

    замени имени session на conn

--- src/repositories.py
+++ src/repositories.py
@@ -5,60 +5,60 @@ from src.sql_queries import (airplanes_query, airports_query, bookings_query, bo
 
 
 def get_airplane_data_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(airplanes_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(airplanes_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
 
 
 def get_airports_data_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(airports_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(airports_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
 
 
 def get_boarding_passes_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(boarding_passes_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(boarding_passes_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
 
 
 def get_bookings_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(bookings_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(bookings_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
 
 
 def get_flights_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(flights_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(flights_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
 
 
 def get_passengers_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(passengers_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(passengers_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
 
 
 def get_routes_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(routes_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(routes_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
 
 
 def get_segments_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(segments_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(segments_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
 
 
 def get_seats_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(seats_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(seats_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
 
 
 def get_tickets_repository(limit: int = 50, offset: int = 0):
-    with get_db_context() as session:
-        result = session.execute(tickets_query, {"limit": limit, "offset": offset})
+    with get_db_context() as conn:
+        result = conn.execute(tickets_query, {"limit": limit, "offset": offset})
         return result.mappings().all()
