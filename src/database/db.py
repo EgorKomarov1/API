a9b7a7eb04fed6011f8df8231bb7ddd9e4400912
Egor Komarov <egor.anakonda10@mail.ru>
2026-08-13 17:10:15 +0300

    создание get_sql

--- src/database/db.py
+++ src/database/db.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from src.config import database_url
+from pathlib import Path
 
 
 engine = create_engine(
@@ -8,3 +9,13 @@ engine = create_engine(
     max_overflow=20,
     pool_pre_ping=True
 )
+
+sql_dir = Path(__file__).parent / 'sql_queries'
+
+
+def get_sql(filename: str) -> text:
+    filename = filename if filename.endswith('.sql') else f'{filename}.sql'
+    sql_path = sql_dir / filename
+    with open(sql_path, 'r') as sql_file:
+        sql = sql_file.read()
+    return text(sql)
