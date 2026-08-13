b0b5a0a8eb24b0fc45228d891d5f4c01b4dee53d
Egor Komarov <egor.anakonda10@mail.ru>
2026-08-13 17:09:01 +0300

    улучшение logger

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,31 @@
 import logging
+import colorlog
+import sys
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+
+def setup_logger():
+    logger = logging.getLogger(__name__)
+
+    logger.setLevel(logging.INFO)
+
+    log_colors = {
+        'DEBUG': 'cyan',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'purple',
+    }
+
+    formatter = colorlog.ColoredFormatter(
+        fmt='%(log_color)s[%(asctime)s] %(levelname)-8s %(filename)s: %(lineno)d - %(message)s',
+        log_colors=log_colors
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+logger = setup_logger()
