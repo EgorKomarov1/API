4becfa39e2e917275fcc9c8024c5f55a3a110e29
Egor Komarov <egor.anakonda10@mail.ru>
2026-08-19 12:18:25 +0300

    изменение middleware

--- src/middlewares/process_time.py
+++ src/middlewares/process_time.py
@@ -3,7 +3,7 @@ from fastapi import Request
 from src.logger import logger
 
 
-async def add_process_time_header(request: Request, call_next):
+async def log_process_time(request: Request, call_next):
     start_time = time.perf_counter()
 
     response = await call_next(request)
